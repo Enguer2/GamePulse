306d9f0d03870187aaeb89ff44c31b0fb3793e8c
Enguer2 <enguerrand.van-de-velde@epitech.eu>
2026-04-13 17:21:32 +0200

    [+] futur features for img

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,12 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com", 
-      "shared.akamai.steamstatic.com",
-      "cdn.akamai.steamstatic.com"
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'shared.akamai.steamstatic.com' },
+      { protocol: 'https', hostname: 'cdn.akamai.steamstatic.com' }
     ],
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.INTERNAL_API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
