3b9221b51740abbc6b64715e9002a628cccc4f8d
Enguer2 <enguerrand.van-de-velde@epitech.eu>
2026-04-13 15:05:08 +0200

    [~] Fixing issues .next

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-  domains: ["lh3.googleusercontent.com"],
-}
+    domains: [
+      "lh3.googleusercontent.com", 
+      "shared.akamai.steamstatic.com",
+      "cdn.akamai.steamstatic.com"
+    ],
+  }
 };
 
 export default nextConfig;
-
